730bc021d41c3ee155b4e0a1465f1f85cae53d92
Ronald Stephens <rstephens@bluemorpho.cc>
2019-08-05 21:15:24 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    array.each do |element|
+      if element.split.include?(@word.split)
+        element
+      end
+  end
+end
+
