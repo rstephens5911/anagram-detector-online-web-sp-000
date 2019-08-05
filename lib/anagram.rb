71f2c84e55575628d8b314eb81492c6c04ca5eb5
Ronald Stephens <rstephens@bluemorpho.cc>
2019-08-05 21:22:59 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,9 +6,17 @@ class Anagram
   end
 
   def match(array)
+    word_array = []
+    word_array << @word.split
+    word_array.sort
+    
     array.each do |element|
-      if element.split.sort == @word.split.sort
-        element
+      element_array = []
+      element_array << element.split
+      element_array.sort
+
+      if element_array == word_array
+        return element
       end
     end
   end
